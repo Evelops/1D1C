f9e2e1cd8c6b42a6f61bba46ebf85167b497d3ab
eomseungyeol <co6747@naver.com>
2021-07-14 20:14:31 +0900

    .

--- JS_project_1/JS/todo.js
+++ JS_project_1/JS/todo.js
@@ -2,12 +2,22 @@ const toDoForm=document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList=document.getElementById("todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement; // event.target => 선택된 객체의 대한 정보 그 각 버튼에 대한 property를 찾아서 각각의 객체가 가지고 있는 위치를 찾아낼 수 있고 그것을 다음과 같이 명시하여 각 li에 추가된 객체를 삭제할 수 있는 함수.
+    li.remove();
+}
+
+
 
 function paintToDo(newTodo){
  const li=document.createElement("li");
  const span=document.createElement("span");
- li.appendChild(span);
  span.innerText=newTodo;
+ const button = document.createElement("button"); 
+ button.innerText="✂";
+ button.addEventListener("click",deleteToDo);
+ li.appendChild(span);
+ li.appendChild(button);
  toDoList.appendChild(li);
 }
 function handleToDoSubmit(event){
