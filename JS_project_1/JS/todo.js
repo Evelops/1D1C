8ab614c0a1deaddb42a26b0f499ce9728d774b56
eomseungyeol <co6747@naver.com>
2021-07-14 19:41:43 +0900

    second commit

--- JS_project_1/JS/todo.js
+++ JS_project_1/JS/todo.js
@@ -2,13 +2,20 @@ const toDoForm=document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList=document.getElementById("todo-list");
 
-function handleToDoSubmit(event){
-    event.preventDefault();
-    const newTodo = toDoInput.value;
-    console.log(toDoInput.value);
-    toDoInput.value="";
-    console.log(newTodo, toDoInput.value);
 
+function paintToDo(newTodo){
+ const li=document.createElement("li");
+ const span=document.createElement("span");
+ li.appendChild(span);
+ span.innerText=newTodo;
+ toDoList.appendChild(li);
+}
+function handleToDoSubmit(event){
+    event.preventDefault(); // input 형식은 enter 눌렀을때 새로고침 되는데 이를 방지하기 위해서 preventDefault 값을 설정.
+    const newTodo = toDoInput.value; // newTodo라는 값에 toDoInput의 value 값을 저장하고 그 값을 다시 비워주기 위해서 다음과 같이 설정.
+    toDoInput.value="";
+    paintToDo(newTodo);
 
 }
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
