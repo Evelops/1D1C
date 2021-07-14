2cd718289d8899dd4c7d381c1375cb80409668f9
eomseungyeol <co6747@naver.com>
2021-07-14 20:30:00 +0900

    .

--- JS_project_1/JS/todo.js
+++ JS_project_1/JS/todo.js
@@ -2,6 +2,14 @@ const toDoForm=document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList=document.getElementById("todo-list");
 
+const toDos =[];
+
+function saveToDos(){
+    localStorage.setItem("todos",JSON.stringify(toDos)); // 그냥 localStroage의 todos 라는 로컬 저장소를 만들고 그 안에 값을 넣어주는 역할을 하는 함수 .
+
+}
+
+
 function deleteToDo(event){
     const li = event.target.parentElement; // event.target => 선택된 객체의 대한 정보 그 각 버튼에 대한 property를 찾아서 각각의 객체가 가지고 있는 위치를 찾아낼 수 있고 그것을 다음과 같이 명시하여 각 li에 추가된 객체를 삭제할 수 있는 함수.
     li.remove();
@@ -24,7 +32,9 @@ function handleToDoSubmit(event){
     event.preventDefault(); // input 형식은 enter 눌렀을때 새로고침 되는데 이를 방지하기 위해서 preventDefault 값을 설정.
     const newTodo = toDoInput.value; // newTodo라는 값에 toDoInput의 value 값을 저장하고 그 값을 다시 비워주기 위해서 다음과 같이 설정.
     toDoInput.value="";
+    toDos.push(newTodo); // toDos라는 배열값에 Todo객체를 하나 입력할 때마다 push .
     paintToDo(newTodo);
+    saveToDos();
 
 }
 toDoForm.addEventListener("submit",handleToDoSubmit);
